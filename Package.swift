71236efc911d5580fbf4d53748f38ad22ef00a2d
Andreas Bauer <andreas.bauer@stanford.edu>
2024-06-06 19:44:37 +0200

    Initial Project Setup (#1)
    # Initial Project Setup
    
    ## :recycle: Current situation & Problem
    This PR marks the start of the SpeziNetworking package. It includes the
    `ByteCoding` target previously located in `SpeziFileFormats`. Together
    with UInt24 support previously part of `ByteCoding` and the `MedFloat16`
    implementation currently located in `BluetoothServices` of
    `SpeziBluetooth`, it forms the new `SpeziNumerics` target for networking
    related numeric types or type support.
    
    ## :gear: Release Notes 
    * Add `ByteCoding` target.
    * Add `SpeziNumerics` target with 24 bit Integer support and MedFloat16
    implementation.
    * Initialized repo with updated name, badges and documentation.
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version:5.9
 
 //
-// This source file is part of the TemplatePackage open source project
-// 
-// SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
-// 
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
 // SPDX-License-Identifier: MIT
 //
 
@@ -12,25 +12,56 @@ import PackageDescription
 
 
 let package = Package(
-    name: "TemplatePackage",
+    name: "SpeziNetworking",
     platforms: [
-        .iOS(.v17),
-        .watchOS(.v10),
+        .iOS(.v16),
+        .watchOS(.v9),
         .visionOS(.v1),
-        .tvOS(.v17),
-        .macOS(.v14)
+        .macOS(.v13),
+        .tvOS(.v16)
     ],
     products: [
-        .library(name: "TemplatePackage", targets: ["TemplatePackage"])
+        .library(name: "ByteCoding", targets: ["ByteCoding"]),
+        .library(name: "XCTByteCoding", targets: ["XCTByteCoding"]),
+        .library(name: "SpeziNumerics", targets: ["SpeziNumerics"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.59.0")
     ],
     targets: [
         .target(
-            name: "TemplatePackage"
+            name: "ByteCoding",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio")
+            ]
+        ),
+        .target(
+            name: "XCTByteCoding",
+            dependencies: [
+                .target(name: "ByteCoding")
+            ]
+        ),
+        .target(
+            name: "SpeziNumerics",
+            dependencies: [
+                .target(name: "ByteCoding"),
+                .product(name: "NIO", package: "swift-nio")
+            ]
         ),
         .testTarget(
-            name: "TemplatePackageTests",
+            name: "ByteCodingTests",
             dependencies: [
-                .target(name: "TemplatePackage")
+                .target(name: "ByteCoding"),
+                .target(name: "XCTByteCoding")
+            ]
+        ),
+        .testTarget(
+            name: "SpeziNumericsTests",
+            dependencies: [
+                .target(name: "ByteCoding"),
+                .target(name: "SpeziNumerics"),
+                .target(name: "XCTByteCoding")
             ]
         )
     ]
